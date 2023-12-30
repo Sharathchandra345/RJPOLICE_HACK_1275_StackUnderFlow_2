5a4d4a62bfc1b953b8d0daf9a47b9f93d5d3992d
Hushraj Singh <saini.hck@gmail.com>
2023-12-30 21:28:25 +0530

    docs added

--- ar_mobile_app/lib/homepage.dart
+++ ar_mobile_app/lib/homepage.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-// import 'dart:io';
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:camera/camera.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -12,24 +12,30 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+  ARSession? arSession;
+  ARModel? groupOfPeople;
 
   @override
   void initState() {
     super.initState();
     _controller = CameraController(
       CameraDescription(
-          name: '0',
-          lensDirection: CameraLensDirection.back,
-          sensorOrientation: 0),
+        name: '0',
+        lensDirection: CameraLensDirection.back,
+        sensorOrientation: 0,
+      ),
       ResolutionPreset.medium,
     );
 
     _initializeControllerFuture = _controller.initialize();
+    arSession = ARSession();
+    groupOfPeople = ARModel("person_.gltf");
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    arSession?.dispose();
     super.dispose();
   }
 
@@ -51,28 +57,42 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   Future<void> _analyzeFrame(String base64Image) async {
-    final response = await http.post(
-      Uri.parse('http://192.168.43.44:5000/analyze'),
-      body: jsonEncode({'image': base64Image}),
-      headers: {'Content-Type': 'application/json'},
-    );
+    try {
+      final response = await http.post(
+        Uri.parse('http://192.168.29.44:5000/analyze'),
+        body: jsonEncode({'image': base64Image}),
+        headers: {'Content-Type': 'application/json'},
+      );
 
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print('Error: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        // Place the 3D model if desired.
+        arSession?.placeModel(groupOfPeople!);
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Camera')),
+      appBar: AppBar(title: Text('AR Camera')),
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
+            return Stack(
+              children: [
+                ARView(
+                  onARViewCreated: (arView) {
+                    arSession?.start();
+                  },
+                ),
+                CameraPreview(_controller),
+              ],
+            );
           } else {
             return Center(child: CircularProgressIndicator());
           }
